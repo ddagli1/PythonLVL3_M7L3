9ac5c337a215b26cb8fc715161ba454332faabe5
Sena2303 <senosss2303@gmail.com>
2026-04-12 21:17:28 +0300

    Şifrelerin farklılığnı test eden fonksiyon

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -24,4 +24,7 @@ def test_password_len():
 #2
 def test_password_is_unique():
     """Arka arkaya oluşturulan iki şifrenin farklı olup olmadığını test eder"""
+    password1 = generate_password(10)
+    password2 = generate_password(10)
+    assert password1 != password2
 
