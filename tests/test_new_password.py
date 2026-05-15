6b710f84687b496c3a576e0d72ce672c1413a1bd
Deniz🌊 <kdendag@gmail.com>
2026-05-15 18:14:50 +0300

    Update test_new_password.py

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -24,13 +24,5 @@ Aşağıda önerilenlerden birini kullanarak başka bir test yazın. Alternatif 
 1. Şifrenin uzunluğunun belirtilen uzunlukla eşleşip eşleşmediğini test edin  
 2. Arka arkaya oluşturulan iki şifrenin farklı olup olmadığını test edin 
 """
-#1
-def test_password_len():
-    """Şifrenin uzunluğunun belirtilen uzunlukla eşleşip eşleşmediğini test eder"""
-    for length in range(1,21):
-        assert length == len(generate_password(length))
-    
-#2
-def test_password_is_unique():
-    """Arka arkaya oluşturulan iki şifrenin farklı olup olmadığını test eder"""
+
 
